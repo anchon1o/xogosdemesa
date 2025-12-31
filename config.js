04bf9ff5d9c6db0c33f8c3caa2de00beb53050e6
anchon1o <anchonio@gmail.com>
2025-12-31 20:25:49 +0100

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Supabase Dashboard → Project Settings → API
 // Pega aquí o Project URL e o anon public key (NON uses service_role no frontend)
-window.SUPABASE_URL = "nnumphbwhdjbwcdqjsli";
+window.SUPABASE_URL = "https://nnumphbwhdjbwcdqjsli.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5udW1waGJ3aGRqYndjZHFqc2xpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxNjQ0MzYsImV4cCI6MjA4Mjc0MDQzNn0.1atKwtpgJaDIPClt32MHee9Cr047URO48bPJg_z0uAY";
